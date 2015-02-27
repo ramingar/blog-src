48a91f130d7289105ae5597af75d856225b918f7
Rafael Minguet <rminguet@gmail.com>
2015-02-27 22:42:13 +0100

    Added node and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('./package.json'),
+    
+    assemble: {
+      options: {
+        layout: 'main.hbs',
+        layoutdir: './public/layouts/',
+        partials: './public/partials/**/*.hbs',
+        collections: [{
+          name: 'posts',
+          inflection: 'post',
+          sortby: 'posted',
+          sortorder: 'desc'
+        }],
+        helpers: './helpers/**/*.js',
+        defaultTitle: 'Rafael Minguet\'s Blog'
+      },
+      skeleton: {
+        files: [{
+          cwd: './public',
+          dest: '../dist/',
+          expand: true,
+          src: ['**/*.hbs', '**/*.md']
+        }]
+      },
+    }
+  });
+
+  /* load every plugin in package.json */
+  grunt.loadNpmTasks('assemble');
+
+  /* grunt tasks */
+  grunt.registerTask('default', ['assemble']);
+
+};
