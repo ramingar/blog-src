b0b6b69eb741c47af6323c943205c81b5a1b2333
Rafael Minguet <rminguet@gmail.com>
2015-03-01 15:29:17 +0100

    Name changed

--- helpers/monthInSpanish.js
+++ helpers/monthInSpanish.js
@@ -2,7 +2,7 @@
 module.exports.register = function (Handlebars, options) {
   'use strict';
 
-  Handlebars.registerHelper('monthSpanish', function (month) {
+  Handlebars.registerHelper('monthInSpanish', function (month) {
     var months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio',
                   'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
     return months[parseInt(month, 10) - 1];
